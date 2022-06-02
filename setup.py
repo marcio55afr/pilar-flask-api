1d13755d1a0e88e5173c487ef9da2cf374170d6f
Marcio Antonio <marcio55afr@gmail.com>
2022-06-02 13:58:24 -0300

    Configuring environment variable

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     zip_safe=False,
     install_requires=[
         'Flask==2.1.2',
-        'pytest==7.1.2'
+        'pytest==7.1.2',
+        'pytest-cov==3.0.0',
+        'pytest-cov==0.20.0'
     ]
 )
